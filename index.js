667a6c3c6572b3f3f4bc1cb3e9a80fd7a41a59f5
Bipul Dey <bipuldey19@gmail.com>
2022-02-26 22:48:38 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 
-const {TOKEN, SERVER_URI} = process.env
+const {TOKEN, SERVER_URL} = process.env
 const TELEGRAM_API=`https://api.telegram.org/bot${TOKEN}`
 const URI = `/webhook/${TOKEN}`
-const WEBHOOK_URL = SERVER_URI+URI
+const WEBHOOK_URL = SERVER_URL+URI
 
 const app = express()
 app.use(bodyParser.json())
