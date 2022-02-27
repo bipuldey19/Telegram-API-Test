ae834f61ba7722c1df3306576b29d443cbfee102
Bipul Dey <bipuldey19@gmail.com>
2022-02-27 18:30:54 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 require('dotenv').config()
 const express = require('express')
-const bodyParser = require('body-parser')
 const axios = require('axios')
+const bodyParser = require('body-parser')
 
-const {TOKEN, SERVER_URL} = process.env
-const TELEGRAM_API=`https://api.telegram.org/bot${TOKEN}`
+const { TOKEN, SERVER_URL } = process.env
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
 const URI = `/webhook/${TOKEN}`
-const WEBHOOK_URL = SERVER_URL+URI
+const WEBHOOK_URL = SERVER_URL + URI
 
 const app = express()
 app.use(bodyParser.json())
 
-const init = async ()=>{
+const init = async () => {
     const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
     console.log(res.data)
 }
 
-app.post(URI, async (req,res) =>{
+app.post(URI, async (req, res) => {
     console.log(req.body)
 
     const chatId = req.body.message.chat.id
     const text = req.body.message.text
 
+  if (text === `/start`){
     await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        chat_Id: chatId,
-        text: text
+        chat_id: chatId,
+        text: `Hello !!!`
     })
-
     return res.send()
+  }
 })
 
-app.listen(process.env.PORT || 5000, async()=>{
-    console.log('App running on port', process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000, async () => {
+    console.log('🚀 app running on port', process.env.PORT || 5000)
     await init()
 })
